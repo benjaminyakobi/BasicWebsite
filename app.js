bb44b2f984f4c622cb627571f91456a5daa8c6c0
benjamin <benjaminyakobi@gmail.com>
2020-08-06 00:08:16 +0300

    basic email/password check functionality

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-// const togglePassword = document.querySelector('#togglePassword');
-// const togglePasswordRpt = document.querySelector('#togglePassword-rpt');
-
-// const password = document.querySelector('#password');
-// const rptpassword = document.querySelector('#psw-repeat');
-
-
-// togglePassword.addEventListener('click', function (e) {
-//     // toggle the type attribute
-//     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-//     password.setAttribute('type', type);
-//     // toggle the eye slash icon
-//     this.classList.toggle('fa-eye-slash');
-// });
-
-// togglePasswordRpt.addEventListener('click', function (e) {
-//     // toggle the type attribute
-//     const type = rptpassword.getAttribute('type') === 'password' ? 'text' : 'password';
-//     rptpassword.setAttribute('type', type);
-//     // toggle the eye slash icon
-//     this.classList.toggle('fa-eye-slash');
-// });
-
 var toggle  = document.getElementById("toggle");
 var content = document.getElementById("content");
 
 toggle.addEventListener("click", function() {
   content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
 });
+
+// var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+if (username == "formget" && password == "formget123"){
+alert ("התחברת בהצלחה");
+// window.location = "success.html"; // Redirecting to other page.
+// return false;
+}
+else{
+  alert ("כתובת מייל או סיסמא שגויים");
+// attempt --;// Decrementing by one.
+// alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+// if( attempt == 0){
+// document.getElementById("username").disabled = true;
+// document.getElementById("password").disabled = true;
+// document.getElementById("submit").disabled = true;
+// return false;
+// }
+}
+}
